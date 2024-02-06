88f1f662750a00e5d3f3b125215b08fe31b9500f
Oleg B <oleg.b@oxtech.org>
2024-02-06 22:22:48 +0200

    translations

---
+++ assets/js/animations/benefits.js
@@ -0,0 +1,6 @@
+import anime from 'animejs/lib/anime.es.js';
+import { fromTop } from './animations';
+export const benefits = () => {
+    const title = document.querySelector('.benefits__title');
+
+}
