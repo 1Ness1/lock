df782ac5530f15f297b75797511b5a3d33f77eaa
Oleg Berlo <oleg.b@oxtech.org>
2024-02-18 12:27:34 +0200

    modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,14 @@ import '../scss/root.scss';
 import { SendToTelegramBot } from './modules/telegram';
 import {observer} from './animations/observer';
 import { burgerToggle } from './animations/burger';
+
+const overflowSet = () => {
+    document.body.classList.add("overflow");
+}
+
+const overflowDisabled = () => {
+    document.body.classList.remove("overflow");
+}
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector(`[data-form]`);
     const telegram = new SendToTelegramBot(form);
@@ -14,12 +22,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     burgerToggle();
 
+
+
     const call = document.querySelectorAll(`[data-call]`);
     const modal = document.querySelector(".modal");
 
     call.forEach(element => {
         element.addEventListener("click", () => {
             modal.classList.add('open');
+            overflowSet();
+        })
+    })
+
+    const modalClose = document.querySelectorAll(`[data-modal="close"]`);
+
+    modalClose.forEach(element => {
+        element.addEventListener('click', function(event) {
+            if(event.target.dataset.modal === "close") {
+                modal.classList.remove("open")
+                overflowDisabled();
+            }
         })
     })
 })
